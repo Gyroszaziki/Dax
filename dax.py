84268f214aee73750057ebb9da2286cc76f00560
Gyroszaziki <l.kleemeier@gmail.com>
2018-05-21 12:07:01 +0200

    add EMA

--- dax.py
+++ dax.py
@@ -7,6 +7,18 @@ df = pd.read_csv("Date-OHLC-Vol-dax-1m.csv", delimiter=';', parse_dates=[['Date'
 print(df.head())
 print(df.info())
 
+
+def EMA( series, n ):
+    ema = series.ewm( span = n ).mean()
+    return( ema )
+
+
+df['EMA']=EMA(df['Close'], 35000)
+print(df.head())
+
+
 plt.figure()
 plt.plot(df["Date_Time"], df["Close"])
-plt.savefig("test.png")
+plt.plot(df["Date_Time"], df["EMA"])
+plt.legend()
+plt.savefig("dax.png")
